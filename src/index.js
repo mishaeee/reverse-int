a77dba5f0d47e788180d447e63003610f627cdac
mishaeee <eliseevmp@gmail>
2021-03-11 00:25:02 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n == 0) {
+        return 0;
+    }
+    if (n < 0) {
+        n = -n;
+    }
+
+    let res = 0;
+    let i = 0;
+    while (n > 0) {
+        res = res * (10 ** i);
+        res += Math.floor(n % 10);
+        n = Math.floor(n / 10);
+        if (i == 0) {
+            i++;
+        }
+    }
+    return res;
 }
